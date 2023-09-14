9f98dd6917196f28999447b73a5b5ab487998190
Archanasingh004 <142914231+Archanasingh004@users.noreply.github.com>
2023-09-14 11:48:57 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+	public static void main(String[] args) 
+	
+	{
+	    String first = "Java"; //String is immutable
+	    String second = "Python";
+	    String third = "Javascript";
+	    
+	    String first = " Hello";  //error
+	    
+		System.out.println(first);
+		System.out.println(second);
+		System.out.println(third);
+	}
+}
